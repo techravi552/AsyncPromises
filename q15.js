ce2eb2f8360bf2aeaa66d9648db06245dd77ccde
Ravi Kalmodiya <ravikalmodiya552gamil.com>
2025-02-08 00:49:02 +0530

    promises

--- q15.js
+++ q15.js
@@ -4,20 +4,20 @@ function registerUser(username, callback) {
 
     setTimeout(() => {
         if (username) {
-            console.log(`User "${username}" registered successfully.`);
+            console.log(`User "${username}" registered successfully.`)
           
         } else {
-            console.log("Registration failed: Username is required.");
+            console.log("Registration failed: Username is required.")
         }
         callback()
-    }, 1000);
+    }, 1000)
 }
 
 function sendVerification(success,username, callback) {
     console.log("Sending verification email...")
 
     setTimeout(() => {
-        // let success = Math.random() > 0.2; 
+    
         if (success) {
             console.log(`Verification email sent to "${username}".`)
        
@@ -30,33 +30,33 @@ function sendVerification(success,username, callback) {
 
 
 function loginUser(success, username, callback ) {
-    console.log("Logging in...");
+    console.log("Logging in...")
 
     setTimeout(() => {
-        // let success = Math.random() > 0.1; // 90% chance of success
+      
         if (success) {
-            console.log(`User "${username}" logged in successfully.`);
+            console.log(`User "${username}" logged in successfully.`)
             callback();
         } else {
-            console.log("Login failed: Incorrect credentials.");
+            console.log("Login failed: Incorrect credentials.")
         }
     }, 1000);
 }
 
 
 function displayWelcomeMessage(username) {
-    console.log("Displaying welcome message...");
+    console.log("Displaying welcome message...")
 
     setTimeout(() => {
-        console.log(`Welcome, ${username}! 🎉`);
-        // callback(null);
+        console.log(`Welcome, ${username}! `)
+      
     }, 1000);
 }
-// registerUser("ravi" , sendVerification(false))
+
 registerUser("ravi" , (()=>{
     sendVerification(true , "ravi" , (()=>{
         loginUser(true ,"ravi" ,(()=>{
-            displayWelcomeMessage("ravi")
+            displayWelcomeMessage("ravi .")
         }))
     }))
 }))
